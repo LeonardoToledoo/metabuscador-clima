ec53ac2687543448bf527ef0ada37ad29e0ed6ca
Leonardo  Toledoo <leonardo.programador20@hotmail.com>
2026-03-24 15:31:37 -0400

    Primeiro commit - projeto inicial

---
+++ src/main/java/com/metaclima/metabuscador/MetabuscadorClimaApplication.java
@@ -0,0 +1,13 @@
+package com.metaclima.metabuscador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetabuscadorClimaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetabuscadorClimaApplication.class, args);
+	}
+
+}

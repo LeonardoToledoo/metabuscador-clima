247cc761341fcf5884621b41138aa4efa3ae639d
andrei.jara <andrei.jara@azi.com.br>
2026-03-30 13:17:23 -0400

    Ajustes no application

--- metabuscadorclima-api/src/main/java/application/MetaClimaApplication.java
+++ metabuscadorclima-api/src/main/java/application/MetaClimaApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication(scanBasePackages = "com.metaclima.backend")
+@SpringBootApplication(scanBasePackages = {"application", "adapter", "domain"})
 @EnableConfigurationProperties({
         WeatherApiProperties.class,
         OpenMeteoProperties.class,
